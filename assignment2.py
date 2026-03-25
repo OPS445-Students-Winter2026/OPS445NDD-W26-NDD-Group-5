ab2483506cd6389dd8374eaea96119cd11418c6c
kk-slider18 <147006428+kk-slider18@users.noreply.github.com>
2026-03-25 15:52:42 -0400

    Update assignment2.py
    rough outline of functionality

--- assignment2.py
+++ assignment2.py
@@ -5,3 +5,12 @@ import argparse
 '''
 Enter Your Code Here
 '''
+def get_ram_usage():
+  # use command 'free' to get snapshot of current state of ram usage in MiB
+
+
+  # extract used, free, total from output
+
+
+def display_ram_usage():
+  # using the data from above, format for display including used and free as percentage of total
