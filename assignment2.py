780506c8001596f00e600585a2c00f438428df87
Jordan Lau <92120992+Jordanlauu@users.noreply.github.com>
2026-04-06 20:08:26 -0400

    completed disk usage functions

--- assignment2.py
+++ assignment2.py
@@ -28,6 +28,20 @@ def display_disk():
 #print the results, converting bytes to GB
     print("hardrive usage")
     print(f"total: {total / (1024 ** 3):.2f} GB")
-    print(f"total: {used / (1024 ** 3):.2f} GB")
+    print(f"used: {used / (1024 ** 3):.2f} GB")
     print(f"free: {free / (1024 ** 3):.2f} GB")
     print(f"percentage used: {percent:.1f} %")
+
+if __name__ == '__main__':
+#creating the phraser
+    parser = argparse.ArgumentParser(description = "Harddrive monitor")
+#added the flag --disk 
+    parser.add_argument('--disk', action='store_true')
+
+#runs the parser and places the extracted data in a argparse object
+    args = parser.parse_args()
+
+#runs the function if --disk was used  
+    if args.disk:
+        display_disk()
+
