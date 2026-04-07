54988149baba622e1e50d30496ec547b5fd75928
Kevin Mitchell <kmitchell34@myseneca.ca>
2026-04-07 00:42:56 -0400

    update comments, minor correction to line 45

--- assignment2.py
+++ assignment2.py
@@ -6,19 +6,19 @@ import argparse
 Enter Your Code Here
 '''
 def get_ram_usage():
-    # read '/proc/meminfo', use split to extract usage in kiB 
-    # express GiB 
-    # 
+    # Use readlines to process '/proc/meminfo', split to isolate desired field  
 
     f = open('/proc/meminfo', 'r')
     sys_stats = f.readlines()
-    rt_raw = sys_stats[0].split()
-    rt = int(rt_raw[1])
-    ra_raw = sys_stats[2].split()
-    ra = int(ra_raw[1])
-    f.close()
+    rt_raw = sys_stats[0].split() # line 1 of meminfo - Total Memory - split to remove excess whitespace 
+    rt = int(rt_raw[1])           # extract second item (memory as kiB) in list
+    ra_raw = sys_stats[2].split() # line 3 of meminfo - Available Memory - split to remove excess whitespace
+    ra = int(ra_raw[1])           # as above, extract memory as kiB
+    f.close()   
 
-    den = 1048576
+    # Convert kiB to GiB
+
+    den = 1048576 # kiB in 1 GiB
 
     total = rt / den
     available = ra / den
@@ -28,8 +28,12 @@ def get_ram_usage():
     return total, available, used, percent
 
 def display_ram_usage():
+    # Call get_ram_usage and process the output for display using an f-string
+
     total, available, used, percent = get_ram_usage()
     print(f'System Memory\nTotal: {total:.2f} GiB\nAvailable: {available:.2f} GiB\nUsed: {used:.2f} GiB\nPercent Used: {percent:.1f}\%')
+
+# Main Block - incl. argparse implementation for RAM function
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description= "RAM monitor")
 
@@ -38,7 +42,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.ram:
-        get_ram_usage()
+        display_ram_usage()
 
 
      
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from assignment2 import get_ram_usage, display_ram_usage
+import assignment2
 
 
-display_ram_usage()
+parser.ram()
